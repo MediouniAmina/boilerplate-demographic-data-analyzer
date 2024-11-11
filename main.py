07e6e70a18f0090c704bf2a5e882fb2ede8a267c
Amina Mediouni <amina.mediouni@esprit.tn>
2024-11-11 00:00:42 +0000

    Demographic Data Analyzer

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-# This entrypoint file to be used in development. Start by reading README.md
+# main.py
+
+# Import the demographic_data_analyzer module, which contains the calculate_demographic_data function
 import demographic_data_analyzer
 from unittest import main
 
-# Test your function by calling it here
+# Test the function by calling it here
+# It will print the demographic analysis results to the console
 demographic_data_analyzer.calculate_demographic_data()
 
 # Run unit tests automatically
+# This will check if the function passes all tests defined in test_module.py
 main(module='test_module', exit=False)
